ecac6a63d1e18afe61a405063a26fdcebb54fc1d
hannah68 <reihaneh.naderi1989@gmail.com>
2022-01-10 11:52:35 +0000

    not completed entirely

--- src/app.js
+++ src/app.js
@@ -6,38 +6,13 @@ import LeftMenu from './components/LeftMenu'
 import initialEmails from './data/emails'
 import './styles/app.css'
 
-const getReadEmails = emails => emails.filter(email => !email.read)
-const getStarredEmails = emails => emails.filter(email => email.starred)
 
 function App() {
-  // const [emails, setEmails] = useState(initialEmails)
   const [state, dispatch] = useReducer(EmailReducer, initialEmails)
 
   // const [hideRead, setHideRead] = useState(false)
-  const [currentTab, setCurrentTab] = useState('inbox')
+  // const [currentTab, setCurrentTab] = useState('inbox')
 
-  // const unreadEmails = emails.filter(email => !email.read)
-  // const starredEmails = emails.filter(email => email.starred)
-
-  // const toggleStar = targetEmail => {
-  //   const updatedEmails = emails =>
-  //     emails.map(email =>
-  //       email.id === targetEmail.id
-  //         ? { ...email, starred: !email.starred }
-  //         : email
-  //     )
-  //   setEmails(updatedEmails)
-  // }
-
-  // const toggleRead = targetEmail => {
-  //   const updatedEmails = emails =>
-  //     emails.map(email =>
-  //       email.id === targetEmail.id ? { ...email, read: !email.read } : email
-  //     )
-  //   setEmails(updatedEmails)
-  // }
-
-  // let filteredEmails = emails
 
   // if (hideRead) filteredEmails = getReadEmails(filteredEmails)
 
